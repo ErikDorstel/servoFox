5e25d6225a80ba33e7e6968a007767af0202a5e1
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2021-11-07 23:58:54 +0100

    minor change

--- WLAN.h
+++ WLAN.h
@@ -19,7 +19,7 @@ void initWLAN() {
   WiFi.onEvent(WiFiStationConnected,SYSTEM_EVENT_STA_CONNECTED);
   WiFi.onEvent(WiFiStationDisconnected,SYSTEM_EVENT_STA_DISCONNECTED);
   WiFi.softAPConfig(IPAddress(192,168,4,1),IPAddress(192,168,4,1),IPAddress(255,255,255,0)); WiFi.softAP(ssidAP,passwordAP);
-  WiFi.setHostname("aurora.tesla"); WiFi.begin(ssidStation.c_str(),passwordStation.c_str());
+  WiFi.setHostname("servoFox"); WiFi.begin(ssidStation.c_str(),passwordStation.c_str());
   if (debug) { Serial.println("WLAN AP with IP address " + WiFi.softAPIP().toString() + " enabled."); }
   tcpServer.begin(); dnsServer.begin(53); }
 
